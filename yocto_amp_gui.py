0311c41749811716b6de32c82aa83d3256f86d4f
heerykk <henry.k.adarkwa@gmail.com>
2018-07-05 18:34:48 +0100

    added combo box

--- yocto_amp_gui.py
+++ yocto_amp_gui.py
@@ -10,80 +10,68 @@ stopping = False
 End = False
 
 class Window(QtGui.QMainWindow):
-    def slow(self):
-        self.sleepTime = 1000
-    def med(self):
-        self.sleepTime = 100
-    def fast(self):
-        self.sleepTime = 10
-    def inf(self):
-        self.sleepTime = 0
-        
-    def Tslow(self):
-        self.timeLength = 60*15
-    def Tmed(self):
-        self.timeLength = 120
-    def Tfast(self):
-        self.timeLength = 60
-    def Tinf(self):
-        self.timeLength = 10
+    
+    def sleepChoice(self, text):
+        if text == "1000":
+            self.sleepTime = 1000
+        elif text == "100":
+            self.sleepTime = 100
+        elif text == "10":
+            self.sleepTime = 10
+        elif text == "0":
+            self.sleepTime = 0
+
+    def timeChoice(self, text):
+        if text == "180":
+            self.timeLength = 180
+        elif text == "120":
+            self.timeLength = 120
+        elif text == "60":
+            self.timeLength = 60
+        elif text == "10":
+            self.timeLength = 10
         
     def __init__(self):
         super(Window,self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("Graph Plotter")
-        self.slow()
-        self.Tfast()
+        self.sleepTime = 1000
+        self.timeLength = 10
 
-        extractAction = QtGui.QAction("1000", self)
-        extractAction.triggered.connect(self.slow)
-        extractAction1 = QtGui.QAction("100", self)
-        extractAction1.triggered.connect(self.med)
-        extractAction2 = QtGui.QAction("10", self)
-        extractAction2.triggered.connect(self.fast)
-        extractAction3 = QtGui.QAction("0", self)
-        extractAction3.triggered.connect(self.inf)
+        comboBox = QtGui.QComboBox(self)
+        comboBox.addItem("1000")
+        comboBox.addItem("100")
+        comboBox.addItem("10")
+        comboBox.addItem("0")
+        comboBox.move(50, 250)
+        comboBox.activated[str].connect(self.sleepChoice)
 
-        extractActionT = QtGui.QAction("180", self)
-        extractActionT.triggered.connect(self.Tslow)
-        extractActionT1 = QtGui.QAction("120", self)
-        extractActionT1.triggered.connect(self.Tmed)
-        extractActionT2 = QtGui.QAction("60", self)
-        extractActionT2.triggered.connect(self.Tfast)
-        extractActionT3 = QtGui.QAction("10", self)
-        extractActionT3.triggered.connect(self.Tinf)
+        comboBox1 = QtGui.QComboBox(self)
+        comboBox1.addItem("180")
+        comboBox1.addItem("120")
+        comboBox1.addItem("60")
+        comboBox1.addItem("10")
+        comboBox1.move(200, 250)
+        comboBox1.activated[str].connect(self.timeChoice)
         
-        self.statusBar()
-        mainMenu = self.menuBar()
-        sleepMenu = mainMenu.addMenu('&Sleep')
-        sleepMenu.addAction(extractAction)
-        sleepMenu.addAction(extractAction1)
-        sleepMenu.addAction(extractAction2)
-        sleepMenu.addAction(extractAction3)
-        
-        TimeMenu = mainMenu.addMenu('&Time')
-        TimeMenu.addAction(extractActionT)
-        TimeMenu.addAction(extractActionT1)
-        TimeMenu.addAction(extractActionT2)
-        TimeMenu.addAction(extractActionT3)
         self.home()
 
     def home(self):
         btn1 = QtGui.QPushButton("Start", self)
         btn1.clicked.connect(self.ampCounter)
         btn1.resize(100,50)
-        btn1.move(200,80)
+        btn1.move(200,20)
 
         btn2 = QtGui.QPushButton("Display", self)
         btn2.clicked.connect(self.ampPlot)
         btn2.resize(100,50)
-        btn2.move(200,160)
+        btn2.move(200,100)
         
         btn3 = QtGui.QPushButton("Quit", self)
         btn3.clicked.connect(QtCore.QCoreApplication.instance().quit)
         #btn3.clicked.connect(self.over)
         btn3.resize(100,50)
-        btn3.move(200,240)
+        btn3.move(200,180)
 
         #self.progress = QtGui.QProgressBar(self)
         #self.progress.setGeometry(200,80,250,20)
