1db0d0b08bcb2fde50a4412beaf2bada79811d0d
heerykk <henry.k.adarkwa@gmail.com>
2018-07-06 18:54:52 +0100

    working start/stop via threading

--- yocto_amp_gui.py
+++ yocto_amp_gui.py
@@ -6,7 +6,7 @@ from yocto_api import *
 from yocto_current import *
 import threading
 
-start = True
+start = False
 stopping = False
 End = False
 xtime = []
@@ -14,18 +14,13 @@ ymA = []
 
 class Window(QtGui.QMainWindow):
     
-    def sleepChoice(self, text):
-        self.sleepTime = int(text)
 
-    def timeChoice(self, text):
-        self.timeLength = int(text)
         
     def __init__(self):
         super(Window,self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle("Graph Plotter")
-        self.sleepTime = 1000
-        self.timeLength = 10
+
 
         comboBox = QtGui.QComboBox(self)
         comboBox.addItem("1000")
@@ -33,7 +28,8 @@ class Window(QtGui.QMainWindow):
         comboBox.addItem("10")
         comboBox.addItem("0")
         comboBox.move(50, 250)
-        comboBox.activated[str].connect(self.sleepChoice)
+        comboBox.activated[str].connect(worker.sleepChoice)
+        comboBox.setEditable(True)
 
         comboBox1 = QtGui.QComboBox(self)
         comboBox1.addItem("180")
@@ -41,13 +37,14 @@ class Window(QtGui.QMainWindow):
         comboBox1.addItem("60")
         comboBox1.addItem("10")
         comboBox1.move(200, 250)
-        comboBox1.activated[str].connect(self.timeChoice)
+        comboBox1.activated[str].connect(worker.timeChoice)
+        comboBox1.setEditable(True)
         
         self.home()
 
     def home(self):
         btn1 = QtGui.QPushButton("Start", self)
-        btn1.clicked.connect(self.ampCounter)
+        btn1.clicked.connect(worker.startWork)
         btn1.resize(100,50)
         btn1.move(200,20)
 
@@ -55,7 +52,12 @@ class Window(QtGui.QMainWindow):
         btn2.clicked.connect(self.ampPlot)
         btn2.resize(100,50)
         btn2.move(200,100)
-        
+
+        btns = QtGui.QPushButton("stop", self)
+        btns.clicked.connect(worker.stop)
+        btns.resize(100,50)
+        btns.move(200,100)
+
         btn3 = QtGui.QPushButton("Quit", self)
         btn3.clicked.connect(QtCore.QCoreApplication.instance().quit)
         #btn3.clicked.connect(self.over)
@@ -65,8 +67,46 @@ class Window(QtGui.QMainWindow):
         #self.progress = QtGui.QProgressBar(self)
         #self.progress.setGeometry(200,80,250,20)
         
-
         self.show()
+
+    def ampPlot(self):
+        stopping = True
+        #self.ampCounter()
+        xtime = []
+        ymA = []
+        with open('data_'+str(int(self.starttime))+'.csv','r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            for row in plots:
+                xtime.append(float(row[0]))
+                ymA.append(float(row[1]))
+        plt.plot(xtime,ymA)
+        plt.show()
+
+        
+class workerThread(threading.Thread):
+
+    def sleepChoice(self, text):
+        self.sleepTime = int(text)
+
+    def timeChoice(self, text):
+        self.timeLength = int(text)
+        
+    def startWork(self):
+        self.go = True
+        
+    def stop(self):
+        self.go = False
+        
+    def __init__(self):
+        self.go = False
+        self.sleepTime = 1000
+        self.timeLength = 10
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            while self.go:
+                self.ampCounter()
 
     def ampCounter(self):
         self.starttime = time.time()
@@ -86,7 +126,8 @@ class Window(QtGui.QMainWindow):
         m = sensor.get_module()
         sensorDC = YCurrent.FindCurrent(m.get_serialNumber() + '.current1')
         print (sensorDC)
-        with open('data_'+str(int(self.starttime))+'.csv', "w") as f:
+        #with open('data_'+str(int(self.starttime))+'.csv', "w") as f:
+        with open('test.csv', "w") as f:
             while sensor.isOnline() and time.time() < (self.starttime + self.timeLength):
             #while stopping == False:
                 timex = time.time() - self.starttime
@@ -120,13 +161,20 @@ class Window(QtGui.QMainWindow):
     def over(self):
         end = True
         start = False
-        
+
+    
     
 def run():
+    #print ("test")
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
     app.exec_()
 
+#p = Window(self)
+#makeT = threading.Thread(target= run,args=())
+#plotT = threading.Thread(target= p.ampPlot,args=())
+#plotT.start
+#makeT.start
 run()
 
     
