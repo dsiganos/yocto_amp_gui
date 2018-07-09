62c2a6717b04f8292c8be1911c8d68ff474add7c
heerykk <henry.k.adarkwa@gmail.com>
2018-07-09 16:56:34 +0100

    made more improvents to code

--- yocto_amp_gui.py
+++ yocto_amp_gui.py
@@ -20,110 +20,91 @@ class Window(QtGui.QMainWindow):
         self.setWindowTitle("Graph Plotter")
 
 
-        comboBox = QtGui.QComboBox(self)
-        comboBox.addItem("1000")
-        comboBox.addItem("100")
-        comboBox.addItem("10")
-        comboBox.addItem("0")
-        comboBox.move(50, 250)
-        comboBox.activated[str].connect(worker.sleepChoice)
-        comboBox.setEditable(True)
+        sleepCombo = QtGui.QComboBox(self)
+        sleepCombo.addItem("1000")
+        sleepCombo.addItem("100")
+        sleepCombo.addItem("10")
+        sleepCombo.addItem("0")
+        sleepCombo.move(50, 250)
+        sleepCombo.activated[str].connect(worker.sleepTimeSet)
+        sleepCombo.setEditable(True)
 
-        comboBox1 = QtGui.QComboBox(self)
-        comboBox1.addItem("180")
-        comboBox1.addItem("120")
-        comboBox1.addItem("60")
-        comboBox1.addItem("10")
-        comboBox1.move(200, 250)
-        comboBox1.activated[str].connect(worker.timeChoice)
-        comboBox1.setEditable(True)
-        
-        self.home()
+        timeCombo = QtGui.QComboBox(self)
+        timeCombo.addItem("180")
+        timeCombo.addItem("120")
+        timeCombo.addItem("60")
+        timeCombo.addItem("10")
+        timeCombo.move(200, 250)
+        timeCombo.activated[str].connect(worker.timeLengthSet)
+        timeCombo.setEditable(True)
 
-    def home(self):
-        btn1 = QtGui.QPushButton("Start", self)
-        btn1.clicked.connect(worker.startWork)
-        btn1.resize(100,50)
-        btn1.move(200,20)
+        startBtn = QtGui.QPushButton("Start", self)
+        startBtn.clicked.connect(worker.startWork)
+        startBtn.resize(100,50)
+        startBtn.move(200,20)
 
-        btn2 = QtGui.QPushButton("Display", self)
-        btn2.clicked.connect(worker.ampPlot)
-        btn2.resize(100,50)
-        btn2.move(200,100)
+        displayBtn = QtGui.QPushButton("Display", self)
+        displayBtn.clicked.connect(worker.plotGraph)
+        displayBtn.resize(100,50)
+        displayBtn.move(200,100)
 
-##        btnl = QtGui.QPushButton("live", self)
-##        btnl.clicked.connect(worker.livePlot)
-##        btnl.resize(100,50)
-##        btnl.move(350,100)
-
-        btns = QtGui.QPushButton("stop", self)
-        btns.clicked.connect(worker.stop)
-        btns.resize(100,50)
-        btns.move(350,20)
-
-        btn3 = QtGui.QPushButton("Quit", self)
-        btn3.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        btn3.resize(100,50)
-        btn3.move(200,180)
+        stopBtn = QtGui.QPushButton("stop", self)
+        stopBtn.clicked.connect(worker.stopWork)
+        stopBtn.resize(100,50)
+        stopBtn.move(350,20)
         
         self.show()
 
         
-class workerThread(threading.Thread):
+class WorkerThread(threading.Thread):
 
-    def sleepChoice(self, text):
+    def sleepTimeSet(self, text):
         self.sleepTime = int(text)
 
-    def timeChoice(self, text):
+    def timeLengthSet(self, text):
         self.timeLength = int(text)
         
     def startWork(self):
         self.go = True
         
-    def stop(self):
+    def stopWork(self):
         self.go = False
         
     def __init__(self):
         self.go = False
         self.sleepTime = 1000
         self.timeLength = 10
+        self.die = False
         threading.Thread.__init__(self)
 
     def run(self):
-        while True:
+        while not self.die:
             while self.go:
-                self.ampCounter()
+                self.collectValues()
+        print("thread is dead")
 
-    def ampCounter(self):
+    def collectValues(self):
         self.starttime = time.time()
         timex = 0
         errmsg = YRefParam()
         target = 'any'
         if YAPI.RegisterHub("usb", errmsg) != YAPI.SUCCESS:
             sys.exit("init error" + errmsg.value)
-        if target == 'any':
-            sensor = YCurrent.FirstCurrent()
-            if sensor is None:
-                print('No module connected')
-        else:
-            sensor = YCurrent.FindCurrent(target + '.current1')
-        if not sensor.isOnline():
-            print('Module not connected')
+        sensor = YCurrent.FirstCurrent()
         m = sensor.get_module()
         sensorDC = YCurrent.FindCurrent(m.get_serialNumber() + '.current1')
         print (sensorDC)
         with open('data_'+str(int(self.starttime))+'.csv', "w") as f:
-            while sensor.isOnline() and time.time() < (self.starttime + self.timeLength):
-                while self.go:
-                    timex = time.time() - self.starttime
-                    ampval = float(sensorDC.get_currentRawValue())
-                    f.write('%s, %s\n' % (timex, ampval))
-                    print ('%s, %s' % (timex, ampval))
+            while sensor.isOnline() and time.time() < (self.starttime + self.timeLength) and self.go == True:
+                timex = time.time() - self.starttime
+                ampval = float(sensorDC.get_currentRawValue())
+                f.write('%s, %s\n' % (timex, ampval))
+                print ('%s, %s' % (timex, ampval))
+                if self.sleepTime > 0:
                     YAPI.Sleep(self.sleepTime, errmsg)
         YAPI.FreeAPI()
 
-
-    def ampPlot(self):
+    def plotGraph(self):
         stopping = True
         xtime = []
         ymA = []
@@ -134,31 +115,18 @@ class workerThread(threading.Thread):
                 ymA.append(float(row[1]))
         plt.plot(xtime,ymA)
         plt.show()
-    
-    def starting(self):
-        start = True
+
+    def kill(self):
+        self.die = True
         
-    def stoping(self):
-        start = False
-
-    def over(self):
-        end = True
-        start = False
-
-    
-    
 def run():
-    #print ("test")
+    global worker
+    worker = WorkerThread()
+    worker.start()
     app = QtGui.QApplication(sys.argv)
     GUI = Window()
     app.exec_()
+    worker.kill()
 
-#p = Window(self)
-#makeT = threading.Thread(target= run,args=())
-#plotT = threading.Thread(target= p.ampPlot,args=())
-#plotT.start
-#makeT.start
+
 run()
-
-    
-
